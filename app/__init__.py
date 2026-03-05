f0598509d7d18cd6976a33c4740211d584bbf3f5
Ujwal L <ujwal@gmail.com>
2026-03-05 19:37:58 +0530

    fix render deployment

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
-from flask_migrate import Migrate  # <-- ADD THIS
-from config import Config
+from flask_migrate import Migrate
+from .config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'routes.login'
 login_manager.login_message_category = 'info'
 csrf = CSRFProtect()
-migrate = Migrate() # <-- ADD THIS
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__, instance_relative_config=True)
@@ -25,15 +25,10 @@ def create_app(config_class=Config):
     db.init_app(app)
     login_manager.init_app(app)
     csrf.init_app(app)
-    migrate.init_app(app, db) # <-- ADD THIS LINE
+    migrate.init_app(app, db)
 
     with app.app_context():
         from . import routes, models
-        
         app.register_blueprint(routes.bp)
-        
-        # NOTE: db.create_all() is now mostly handled by Flask-Migrate 
-        # but we keep it for initial creation if no migrations exist.
-        # db.create_all() 
 
     return app
